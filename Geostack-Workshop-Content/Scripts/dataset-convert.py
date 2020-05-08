5d180277daa59ca581346566b3a32f70e13b2075
The-GeoStack-Project <pieterlems.personal@protonmail.com>
2020-05-08 20:05:09 +0200

    Added centralized Data folder / removed datasets / Added data-convert script

---
+++ Geostack-Workshop-Content/Scripts/dataset-convert.py
@@ -0,0 +1,71 @@
+
+import gpxpy
+import datetime
+import pandas as pd
+import os
+
+input_location_gpx = '/home/geostack/GeoStack-Workshop/Geostack-Workshop-Content/Workshop-Datasets/GPX/'
+output_location_gpx = '/home/geostack/GeoStack-Workshop/Geostack-Workshop-Content/Part-2-Data-storage/Datasets/JSON/'
+
+input_location_csv = '/home/geostack/GeoStack-Workshop/Geostack-Workshop-Content/Workshop-Datasets/CSV/'
+output_location_csv = '/home/geostack/GeoStack-Workshop/Geostack-Workshop-Content/Part-2-Data-storage/Datasets/JSON/'
+
+
+def create_df(data):
+
+    df = pd.DataFrame(columns=['lon', 'lat', 'alt', 'time'])
+
+    for point in data:
+        df = df.append({'lon': point.longitude,
+                        'lat' : point.latitude,
+                        'alt' : point.elevation,
+                        'time' : point.time}, ignore_index=True)
+    return df
+
+def parse_transform_GPX(input_file,output_file):
+
+    input_location = input_location_gpx + input_file
+
+    output_location = output_location_gpx + output_file
+
+    parsed_file =  gpxpy.parse(open(input_location, 'r'))
+
+    df = create_df(parsed_file.tracks[0].segments[0].points)
+
+    df.to_json(output_location,orient='records')
+
+    print("transformed: "+input_file+" to: " + output_file)
+
+
+def filter_transform_CSV(input_file,output_file):
+
+    input_location = input_location_csv+input_file
+
+    output_location = output_location_csv+output_file
+
+    df = pd.read_csv(input_location,low_memory=False)
+
+    columns_to_filter = ['event-id', 'study-name',
+                     'timestamp',
+                     'ground-speed',
+                     'location-long','location-lat',
+                     'height-above-ellipsoid',
+                     'individual-taxon-canonical-name',
+                     'individual-local-identifier']
+
+    filtered_df = df[columns_to_filter]
+
+    filtered_df.to_json(output_location,orient='records')
+
+    print("transformed: "+input_file+" to: " + output_file)
+
+
+
+
+parse_transform_GPX("Zeeland_Car_1.gpx",'Route-Zeeland_Car_1.json')
+parse_transform_GPX("Biesbosch.gpx",'Route-Biesbosch.json')
+parse_transform_GPX("Zeeland_Car_2.gpx",'Route-Zeeland_Car_2.json')
+
+filter_transform_CSV('20181003_Dataset_SV_GPS_Crane_9381_STAW_Crane_RRW-BuGBk_Frida.csv','Crane-Frida.json')
+filter_transform_CSV('20181003_Dataset_SV_GPS_Crane_9407_STAW_Crane_RRW-BuGBk_Agnetha.csv','Crane-Agnetha.json')
+filter_transform_CSV('20181003_Dataset_SV_GPS_Crane_9472_STAW_Crane_RRW-BuGR_Cajsa.csv','Crane-Cajsa.json')
--- Installation-scripts/5-dataset-import.sh
+++ Installation-scripts/5-dataset-import.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+echo "-------------->>>> Copying Workshop Part 1 Datasets <<<<--------------"
+sleep 2
+
+cp -r ~/GeoStack-Workshop/Geostack-Workshop-Content/Workshop-Datasets/CSV ~/GeoStack-Workshop/Geostack-Workshop-Content/Part-1-Data-processing/Datasets/
+cp -r ~/GeoStack-Workshop/Geostack-Workshop-Content/Workshop-Datasets/GPX ~/GeoStack-Workshop/Geostack-Workshop-Content/Part-1-Data-processing/Datasets/
+
+echo "-------------->>>> Transforming and copying datasets for workshop Part 2 <<<<--------------"
+sleep 2
+
+python3 /home/geostack/GeoStack-Workshop/Geostack-Workshop-Content/Scripts/dataset-convert.py
+
+
+
 echo "-------------->>>> Importing and indexing Crane datasets <<<<--------------"
 sleep 2
 
@@ -11,18 +24,6 @@ sleep 2
 
 # Run the Python script used to import all the GPS-Route (Trail) datasets.
 python3 /home/geostack/GeoStack-Workshop/Geostack-Workshop-Content/Scripts/trail-datasets-import.py
-
-echo "-------------->>>> Copying Workshop Part 1 Datasets <<<<--------------"
-sleep 2
-
-cp -r ~/GeoStack-Workshop/Geostack-Workshop-Content/Workshop-Datasets/CSV ~/GeoStack-Workshop/Geostack-Workshop-Content/Part-1-Data-processing/Datasets/
-cp -r ~/GeoStack-Workshop/Geostack-Workshop-Content/Workshop-Datasets/GPX ~/GeoStack-Workshop/Geostack-Workshop-Content/Part-1-Data-processing/Datasets/
-
-echo "-------------->>>> Copying Workshop Part 2 Datasets <<<<--------------"
-sleep 2
-
-cp -r ~/GeoStack-Workshop/Geostack-Workshop-Content/Workshop-Datasets/JSON ~/GeoStack-Workshop/Geostack-Workshop-Content/Part-2-Data-storage/Datasets/
-
 
 
 echo "-------------->>>> DONE <<<<--------------"
